dcfdd3a2eaaae5064b3cc56b6b23221cf8feb478
Damian Nowakowski <Xororian@o2.pl>
2018-03-02 20:42:26 +0100

    reworked, huge performance increase

--- js.js
+++ js.js
@@ -1,58 +1,43 @@
+/*
+	by Damian Nowakowski
+	https://github.com/Nairorox
+
+*/
+
+let imgSrc = 'convertMe.jpeg';	//relative file path
+let verticalMerger = 5;	//scales width, must be positive ingeger
+let horizontalMerger = 5 * verticalMerger;	//scales height, must be positive integer
+const ascii = `@%#*+=-:.`.split('') //used ascii characters 
+
+
+
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 const output = document.querySelector('.output');
-const ascii = `@%#*+=-:.`.split('')
 const pxlLngth = 256/ascii.length;
 const img = new Image();
-
-img.src = "convertMe.jpeg";
-
-let breakCounter  = 0;
-let matrix = [];
-
-let merger = 4;
-let verticalMerger = (merger * 4);
-
-function mergeTimes(matrix, y, x, timesX, timesY){
-	let pixelMerger = 0;
-	//merging horizontal
-	for(var i = x; i < timesX + x; i++){
-		if(matrix[y] && matrix[y][i]){
-			pixelMerger += matrix[y][i];
-		}
-	}
-	//merging vertical
-	for(var i = y; i < timesY + y; i++){
-		if(matrix[i] && matrix[i][x]){
-			pixelMerger += matrix[i][x];
-		}
-	}
-	return (pixelMerger / (timesX + timesY))/pxlLngth;
-}
-
+img.src = imgSrc;
 img.onload = function(){	//main
 	ctx.drawImage(img, 0, 0);
-	let imgData = ctx.getImageData(0,0, img.width, img.height);
-	let pixels = imgData.data;
+	let pixels = ctx.getImageData(0,0, img.width, img.height).data;
 
-	//matrix
-	let matrixRow = [];
-	for(var i = 0; i < pixels.length; i += 4){
-	 let bw = (pixels[i] + pixels[i+1] + pixels[i + 2]) / 3;
-	 matrixRow.push(bw);
-		if(breakCounter >= img.width){
-			breakCounter = 0;
-			matrix.push(matrixRow);
-			matrixRow = [];
-		}
-		breakCounter += 1;
-	}
+	let imageRow = '';
+	let pxCounter = 0
 
-	for(var i=0; i < matrix.length; i+= verticalMerger){
-	let	domRow = document.createElement('p');
-		for(var j = 0; j < matrix[Math.floor(i)].length; j+=merger){
-			domRow.innerText += ascii[Math.floor(mergeTimes(matrix, Math.floor(i), Math.floor(j), merger, verticalMerger))];
+	for(var i = 0; i < Math.floor(img.height / horizontalMerger); i+= 1){
+
+		let paragraph = document.createElement('p');
+		for(var j = 0; j < Math.floor(img.width / verticalMerger); j++){
+			let bw = (pixels[pxCounter] + pixels[pxCounter+1] + pixels[pxCounter + 2]) / 3;
+			imageRow += ascii[Math.floor(bw/pxlLngth)];
+			pxCounter += (4 * verticalMerger);
 		}
-		output.appendChild(domRow);
+		if(img.width /verticalMerger % 1 !== 0){
+			pxCounter +=  ((img.width - Math.floor(img.width / verticalMerger)*verticalMerger)*4); //adding pixels to 
+		}
+		pxCounter += ((horizontalMerger - 1) * img.width * 4);	//adds pixels to counter depending on how many lines are vertically scaled
+		paragraph.innerText = imageRow
+		imageRow = '';
+		output.appendChild(paragraph);
 	}
 };
